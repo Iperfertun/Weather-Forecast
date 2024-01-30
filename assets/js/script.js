cac60047dddbc13e9ae57d7ee0ba7d23a5dbd1df
Tunde <Foltunde@gmail.com>
2024-01-30 19:24:56 +0000

    current waether

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,10 +14,25 @@ function initPage() {
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
  
     // Assigning a unique API to a variable
- const APIKey = "84b79da5e5d7c92085660485702f4ce8";
+ const APIKey = "58b4cabd0fe0f134cd460e0ab1cc2079";
 
  function getWeather(cityName) {
      // Execute a current weather get request from open weather api
      let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
      axios.get(queryURL)
          .then(function (response) {
+            
+            todayweatherEl.classList.remove("d-none");
+
+            // Parse response to display current weather
+            const currentDate = new Date(response.data.dt * 1000);
+            const day = currentDate.getDate();
+            const month = currentDate.getMonth() + 1;
+            const year = currentDate.getFullYear();
+            nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+            let weatherPic = response.data.weather[0].icon;
+            currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+            currentPicEl.setAttribute("alt", response.data.weather[0].description);
+            currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+            currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+            currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
